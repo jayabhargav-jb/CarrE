353b4aa46dad94d07093a89da9abfb60f469cc7b
Rajath <hegderajath28@gmail.com>
2024-10-26 13:45:49 +0530

    First commit

---
+++ pid_control_with_delta_with_serialwrite/header_for_two_motor_control.h
@@ -0,0 +1,71 @@
+#ifndef _header_forpid_H
+#define _header_forpid_H
+
+#define pwm_r1 9
+#define pwm_l1 10
+#define pwm_r2 11
+#define pwm_l2 12
+#define enable_1r 4
+#define enable_1l 5
+#define enable_2r 6
+#define enable_2l 7
+void pulseCounter1();
+void pulseCounter2();
+float speed_calculation(unsigned long int pulse_Counter1,unsigned long int pulse_Counter2);
+int PID_calculations(float rpm1, float rpm2);
+
+float Kp1 = 0.2, Ki1 = 0.01, Kd1 = 0.002;  // PID constants
+float Kp2 = 0.2, Ki2 = 0.01, Kd2 = 0.002;  // PID constants
+
+float error1=0.0,prevError1=0.0;
+float integral1=0.0, derivative1=0.0;
+float setpoint1 = 30.0;  // Desired speeds  
+
+float error2=0.0,prevError2=0.0;
+float integral2=0.0, derivative2=0.0;
+float setpoint2 = 30.0;  // Desired speeds  
+
+//float actualSpeed1, actualSpeed2;  // Feedback from encoders
+float  rpm1=0.0;
+float output1=0.0;
+const int encoderPin1 = 21;  // Encoder 1 signal pin 
+
+//float actualSpeed1, actualSpeed2;  // Feedback from encoders
+float  rpm2=0.0;
+float output2=0.0;
+const int encoderPin2 = 2;  // Encoder 2 signal pin
+
+// Volatile variables to store the pulse counts for each motor
+volatile unsigned int pulseCount1 = 0;  // Pulse count for Motor 1
+volatile unsigned int pulseCount2 = 0;  // Pulse count for Motor 1
+
+unsigned int out1=0;
+unsigned int out2=0;
+
+unsigned long int lastpulseCount1 = 0;
+unsigned long int pulseCountdiff_1;
+float prevrpm1=0.0;
+
+unsigned long int lastpulseCount2 = 0;
+unsigned long int pulseCountdiff_2;
+float prevrpm2=0.0;
+
+const unsigned int pulsesPerRevolution = 7;  // Encoder gives 7 pulses per revolution
+// Variables to handle timing
+unsigned long previousMillis = 0;  // To track time
+const unsigned long interval = 50;  // Interval to calculate RPM (1 second)
+unsigned long int multiplier_constant=(60000/interval); 
+unsigned long previousTime=0;
+//char* cmd_val = (char*)malloc(10 * sizeof(char)); 
+// variables
+//int dir, lpwm, rpwm;
+//int pwm;
+//int last_pwm_r = 0;
+//int last_pwm_l = 0;
+
+unsigned long int  pwmforconversion_1=0;
+unsigned long int  pwmforconversion_2=0;
+
+char* cmd_val = (char*)malloc(10 * sizeof(char)); 
+int dir;
+#endif // _header_forpid_H
---
+++ pid_control_with_delta_with_serialwrite/pid_control_with_delta_with_serialwrite.ino
@@ -0,0 +1,208 @@
+#import "header_for_two_motor_control.h"
+// Interrupt service routine (ISR) for Motor 1
+void pulseCounter1() {
+  pulseCount1++;
+}
+void pulseCounter2() {
+  pulseCount2++;
+}
+
+
+float speed_calculation(unsigned long int pulse_Counter1,unsigned long int pulse_Counter2){
+
+    // Serial.print(lastpulseCount);
+    // Serial.print(" ");
+    // Serial.print(pulseCounter1);
+    // Serial.print(" ");
+    pulseCountdiff_1 = (pulse_Counter1-lastpulseCount1);
+    pulseCountdiff_2 = (pulse_Counter2-lastpulseCount2);
+    // Serial.print(pulseCount);
+    // Serial.print(" ");
+  // Calculate RPM for Motor 1
+    rpm1 = (pulseCountdiff_1*multiplier_constant) / (pulsesPerRevolution*19.1);
+    rpm2 = (pulseCountdiff_2*multiplier_constant) / (pulsesPerRevolution*19.1);
+
+    if(lastpulseCount1>pulseCount1){
+      rpm1=prevrpm1;
+    }
+    if(lastpulseCount2>pulseCount2){
+      rpm2=prevrpm2;
+    }
+
+    lastpulseCount1 = pulseCount1;
+    prevrpm1=rpm1;
+
+    lastpulseCount2 = pulseCount2;
+    prevrpm2=rpm2;
+    //Serial.print("RPM: ");
+    Serial.print(rpm1);
+    Serial.print(",");
+    Serial.println(rpm2);
+    return rpm1,rpm2;
+
+}
+
+
+int PID_calculations(float rpm1, float rpm2){
+   unsigned long currentTime=millis();
+   
+   unsigned long deltaTime=(currentTime-previousTime);
+   error1 = (setpoint1) - (rpm1);
+   error2 = (setpoint2) - (rpm2);
+   // Integral term
+   integral1 += (error1*deltaTime);
+   integral2 += (error2*deltaTime);
+    // Derivative term
+   derivative1 = ((error1 - prevError1)/(deltaTime));
+   derivative2 = ((error2 - prevError2)/(deltaTime));
+
+    // PID output
+   output1 = (Kp1 * error1) + (Ki1 * integral1) + (Kd1 * derivative1);
+   output2 = (Kp2 * error2) + (Ki2 * integral2) + (Kd2 * derivative2);
+   out1=(int)(output1);
+   out2=(int)(output2);
+   if((out1>255)){
+     out1=255;
+   }
+   if((out2>255)){
+     out2=255;
+   }
+   if((out1<0)){
+     out1=0;
+   }
+   if((out2<0)){
+     out2=0;
+   }
+   //if(integral1>30.0){
+    //integral1=27.94;
+   //}
+   prevError1 = error1;
+   prevError2 = error2;
+   previousTime=currentTime;
+
+   return out1,out2;
+}
+
+void setup() {
+  Serial.begin(9600);  // Start serial communication at 9600 baud rate
+  cmd_val[9] = '\0';
+  pinMode(enable_1r,OUTPUT);
+  pinMode(enable_1l,OUTPUT);
+  pinMode(enable_2r,OUTPUT);
+  pinMode(enable_2l,OUTPUT);
+  
+  // Set encoder pins as inputs
+  pinMode(encoderPin1, INPUT);
+  pinMode(encoderPin2, INPUT);
+  pinMode(pwm_r1, OUTPUT);
+  pinMode(pwm_l2, OUTPUT);
+  // setting the pins high
+  digitalWrite(enable_1r,HIGH);
+  digitalWrite(enable_1l,HIGH);
+  digitalWrite(enable_2r,HIGH);
+  digitalWrite(enable_2l,HIGH);
+
+  
+  // Attach interrupts to the encoder pins
+  attachInterrupt(digitalPinToInterrupt(encoderPin1), pulseCounter1, RISING);  // Motor 1 encoder
+  attachInterrupt(digitalPinToInterrupt(encoderPin2), pulseCounter2, RISING); 
+  //analogWrite(pwm_r1, 50);
+  //analogWrite(pwm_r1, 10);
+  //delay(2000);
+
+}
+
+void loop() {
+  
+  unsigned long currentMillis = millis();
+  //while (Serial.available() == 0) {
+
+  //setpoint1 = Serial.parseInt();
+  
+  //Serial.println(setpoint1);
+//}
+  // Check if 1 second (1000 ms) has passed
+  // checks the speed and also calculates the error and finds the pwm required
+  
+  if (Serial.available() > 8) {
+    Serial.readBytesUntil('\n', cmd_val, 9); // Read until newline
+    cmd_val[9] = '\0'; // Ensure null termination
+    // Serial.print("Received: ");
+    // Serial.println(cmd_val);
+    // Parsing the values from the string
+   char* token = strtok(cmd_val, " "); // Split by space
+   if (token) {
+      dir = atoi(token); // Convert first token to integer
+      token = strtok(NULL, " "); // Move to next token
+     }
+   if (token) {
+      setpoint1 = atoi(token); // Convert second token to integer
+      token = strtok(NULL, " "); // Move to next token
+     }
+   if (token) {
+      setpoint2 = atoi(token); // Convert third token to integer
+     }
+     //Serial.flush();
+  }
+    
+
+
+  if ((currentMillis-previousMillis) >= interval) {
+    //Serial.print("1");
+    rpm1,rpm2=speed_calculation(pulseCount1,pulseCount2);
+    out1,out2=PID_calculations(rpm1,rpm2);
+    //Serial.print(out1);
+    //Serial.print(",");
+    //Serial.println(out2);
+    pwmforconversion_1=(out1/1.5);
+    pwmforconversion_2=(out2/1.5);
+    // if ((out1>50)||(out2>50)){       //look into this for the end
+    //   out1=50;
+    //   out2=50;
+
+    // }
+    
+    //analogWrite(pwm_r1,pwmforconversion_1);
+    //analogWrite(pwm_l2,pwmforconversion_2);
+    if (dir==1) {
+       analogWrite(pwm_l1, 0);
+       analogWrite(pwm_r1, pwmforconversion_1);
+       analogWrite(pwm_l2, pwmforconversion_2);
+       analogWrite(pwm_r2, 0);
+     }
+     else if (dir==2) {
+       analogWrite(pwm_l1, pwmforconversion_1);
+       analogWrite(pwm_r1, 0);
+       analogWrite(pwm_l2, 0);
+       analogWrite(pwm_r2, pwmforconversion_2);
+     }
+     else {
+       analogWrite(pwm_l1, 0);
+       analogWrite(pwm_r1, 0);
+       analogWrite(pwm_l2, 0);
+       analogWrite(pwm_r2, 0);
+     }
+
+
+    
+    // Print Motor 1 RPM to the Serial Monitor
+    //Serial.print("Motor 1 Speed: ");
+    
+    // Serial.print(",");
+    // Serial.print("integral ");
+    // Serial.print(integral1);
+    // Serial.print("error ");
+    // Serial.print(error1);
+    // Serial.println(",");
+    
+    
+
+     previousMillis = currentMillis;  //assigning previous time to the current time 
+    
+    // pulseCount1 = 0;   // making the count of the pulse count1 and pulse 2 count =0
+  
+    
+  }
+  
+}
+
